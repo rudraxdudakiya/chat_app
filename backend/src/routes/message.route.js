00e9375f424994d70c4892c17de52f37e91b41a2
rudraxdudakiya <dudakiyarudrax12@gmail.com>
2025-11-25 20:05:10 +0530

    initial commit

---
+++ backend/src/routes/message.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/send", (req, res)=> {
+    res.send("Message Send Endpoint")
+});
+router.get("/receive", (req, res)=> {
+    res.send("Message Receive Endpoint")
+});
+
+export default router;
