f6a710098f506b97acbbaa90ab4b7aec1b56753f
rudraxdudakiya <dudakiyarudrax12@gmail.com>
2025-12-02 21:04:59 +0530

    deployement config....

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios  from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: (import.meta.env.MODE === "development") ? "http://localhost:3000/api" : "/api",
+    baseURL: `${import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"}/api`,
     withCredentials: true,
 });
 
